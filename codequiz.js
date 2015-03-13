6614bb2a4a6f453087b9cb0909ab95e11338033a
Cory Flanigan <cory@lfo.audio>
2015-03-13 13:30:36 -0600

    Begin moving questions to a Mongo collection

--- codequiz.js
+++ codequiz.js
@@ -7,6 +7,84 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+Questions = new Mongo.Collection("questions");
+
+// LOAD QUIZ QUESTIONS
+var questions = [];
+
+questions = [{
+   // what about showing an example? (symbol, e.g. - example: <a href="#"/>)
+     question: "Which method creates an &lt;a&gt; HTML element that is used as a hypertext target?",
+     answer: "anchor",
+     type: "fitb", // what does this connote? what are fitb/mc?
+     difficulty: "easy",
+     language: "javascript"
+  },{
+      question: "Which method returns the specified character from a string?",
+      possibleAnswers: "bind,concat,charAt,slice",
+      answer: "charAt",
+      type: "mc",
+      difficulty: "easy",
+      language: "javascript"
+  },{
+      question: "How would you combine two or <br>more strings without using + ?",
+      answer: "concat",
+      type: "fitb",
+      difficulty: "easy",
+      language: "javascript"
+  },{
+      question: "Which method creates a new array with all elements that pass the test implemented by the provided function?",
+      possibleAnswers: "map,filter,for,join",
+      answer: "filter",
+      type: "mc",
+      difficulty: "easy",
+      language: "javascript"
+  },{
+      question: "How do you return the first index at which a given element can be found in an array, or -1 if it is not present?",
+      possibleAnswers: "indexOf,index,charAt,location",
+      answer: "indexOf",
+      type: "mc",
+      difficulty: "easy",
+      language: "javascript"
+  },{
+      question: "<code>var sampleArray = ['apple', 'banana', 'cherry'];\nvar finalArray = sampleArray[0] + sampleArray[1] + sampleArray[2]; </code> <br><br>What method would better accomplish the above?",
+      answer: "join",
+      type: "fitb",
+      difficulty: "easy",
+      language: "javascript"
+  },{
+      question: "Which method removes the last element from an array and returns that element?",
+      possibleAnswers: "push,shove,shift,pop",
+      answer: "pop",
+      type: "mc",
+      difficulty: "easy",
+      language: "javascript"
+  },{
+      question: "How would you make the first element of an array become the last and the last element of an array become the first?",
+      answer: "reverse",
+      type: "fitb",
+      difficulty: "easy",
+      language: "javascript"
+  },{
+      question: "Which method extracts a section of a string and returns a new string?",
+      possibleAnswers: "splice,split,slice,shift",
+      answer: "slice",
+      type: "mc",
+      difficulty: "easy",
+      language: "javascript"
+  },{
+      question: "Which method returns a string that might 'LOOK LIKE THIS?'",
+      answer: "toUpperCase",
+      type: "fitb",
+      difficulty: "easy",
+      language: "javascript"
+}];
+
+// function createQuestions(questions) {
+//   questions.each {|q| Questions.insert(q)
+// }
+
 // Quiz content, may need to completely re-write
 //     // Quiz Implimentation
     //passed variables: diff currentLang
@@ -20,7 +98,6 @@ if (Meteor.isServer) {
 // }
 
 // var questionList, questionCounter, currentAnswer, mcAnswers, correctQuestions;
-
 
 // var populate = function(qtype){
 //     $('#question-number').text(questionCounter + ' / ' + totalQuest);
@@ -152,53 +229,3 @@ if (Meteor.isServer) {
 //     });
 // });
 
-
-// QUESTIONS TO ADD TO DB, JS -> EASY
-// var easyQuestions, mediumQuestions, hardQuestions = [];
-
-// easyQuestions = [{
-//     question: "Which method creates an &lt;a&gt; HTML element that is used as a hypertext target?",
-//     answer: "anchor",
-//     type: "fitb"
-// },{
-//     question: "Which method returns the specified character from a string?",
-//     possibleAnswers: "bind,concat,charAt,slice",
-//     answer: "charAt",
-//     type: "mc"
-// },{
-//     question: "How would you combine two or <br>more strings without using + ?",
-//     answer: "concat",
-//     type: "fitb"
-// },{
-//     question: "Which method creates a new array with all elements that pass the test implemented by the provided function?",
-//     possibleAnswers: "map,filter,for,join",
-//     answer: "filter",
-//     type: "mc"
-// },{
-//     question: "How do you return the first index at which a given element can be found in an array, or -1 if it is not present?",
-//     possibleAnswers: "indexOf,index,charAt,location",
-//     answer: "indexOf",
-//     type: "mc"
-// },{
-//     question: "<code>var sampleArray = ['apple', 'banana', 'cherry'];\nvar finalArray = sampleArray[0] + sampleArray[1] + sampleArray[2]; </code> <br><br>What method would better accomplish the above?",
-//     answer: "join",
-//     type: "fitb"
-// },{
-//     question: "Which method removes the last element from an array and returns that element?",
-//     possibleAnswers: "push,shove,shift,pop",
-//     answer: "pop",
-//     type: "mc"
-// },{
-//     question: "How would you make the first element of an array become the last and the last element of an array become the first?",
-//     answer: "reverse",
-//     type: "fitb"
-// },{
-//     question: "Which method extracts a section of a string and returns a new string?",
-//     possibleAnswers: "splice,split,slice,shift",
-//     answer: "slice",
-//     type: "mc"
-// },{
-//     question: "Which method returns a string that might 'LOOK LIKE THIS?'",
-//     answer: "toUpperCase",
-//     type: "fitb"
-// }];
