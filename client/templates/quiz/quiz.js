c8536084373b77c4109a892fa95e564f0a65342f
Nick Fredman <ntfredman@gmail.com>
2015-03-16 16:57:49 -0600

    Style changes for homepage, language page, and quiz page

--- client/templates/quiz/quiz.js
+++ client/templates/quiz/quiz.js
@@ -1,22 +1,22 @@
-Template.quiz.quizQuestions = function() {
-  var questions = Questions.find({language: "javascript", difficulty: "easy"}).fetch();
+// Template.quiz.quizQuestions = function() {
+//   var questions = Questions.find({language: "javascript", difficulty: "easy"}).fetch();
 
-  return _.first(_.shuffle(questions), 10);
-};
+//   return _.first(_.shuffle(questions), 10);
+// };
 
-Session.set("currentQuestion", 1);
+// Session.set("currentQuestion", 1);
 
-Template.currentQuestion.helpers({
-})
+// Template.currentQuestion.helpers({
+// })
 
-Template.currentQuestion.events({
-  "click submit": function(e){
-    e.preventDefault();
-    alert("this is the right submit");
-// check answer
-    Session.set("currentQuestion", Session.get("currentQuestion") + 1);
-  }
-});
+// Template.currentQuestion.events({
+//   "click submit": function(e){
+//     e.preventDefault();
+//     alert("this is the right submit");
+// // check answer
+//     Session.set("currentQuestion", Session.get("currentQuestion") + 1);
+//   }
+// });
 
 //entire quiz collection
 //  1) get an array of 10 question objects at random from main quiz collection
