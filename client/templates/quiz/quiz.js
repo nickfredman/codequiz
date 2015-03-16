3882d6e8690fcfba7372874ffc2c3262e63f2094
Nick Fredman <ntfredman@gmail.com>
2015-03-15 22:09:53 -0600

    Begins quiz implementation

---
+++ client/templates/quiz/quiz.js
@@ -0,0 +1,32 @@
+Template.quiz.quizQuestions = function() {
+  var questions = Questions.find({language: "javascript", difficulty: "easy"}).fetch();
+
+  return _.first(_.shuffle(questions), 10);
+};
+
+Session.set("currentQuestion", 1);
+
+Template.currentQuestion.helpers({
+})
+
+Template.currentQuestion.events({
+  "click submit": function(e){
+    e.preventDefault();
+    alert("this is the right submit");
+// check answer
+    Session.set("currentQuestion", Session.get("currentQuestion") + 1);
+  }
+});
+
+//entire quiz collection
+//  1) get an array of 10 question objects at random from main quiz collection
+//  2) ->> currentQuestion
+//  3) # in the array
+//  4) correctAnswers ++
+//  5) Incorrect = wrongAnswers.push(currentQuestion)
+//
+// var quiz = {
+//  user_id:
+//  questions: X number of questions randomly chosen from the Collection
+//  answers: for each of the above questions
+// }
