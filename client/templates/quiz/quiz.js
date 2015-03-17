7856f492bd0032d532f93952daca6f6d7b0763d1
Cory Flanigan <cory@lfo.audio>
2015-03-17 13:03:10 -0600

    Refactor Quiz implementation

--- client/templates/quiz/quiz.js
+++ client/templates/quiz/quiz.js
@@ -1,122 +1,8 @@
-// Quiz = {
-//   getCurrentQuestion: function() {
-//     var question = this.currentQuiz()[this.currentQuestionIndex()];
-// 
-//     // do this on submitting an answer
-//     // this.incrementCurrentQuestion();
-//   
-//     return question;
-//   },
-// 
-//   currentQuestionIndex: function() {
-//     return (Session.get("currentQuestionIndex") || 0);
-//   },
-//   
-//   currentQuiz: function() {
-//     // move this logic to a meteor method on the server (e.g. getQuiz)
-//     var result = {};
-// 
-//     if(Session.get("currentQuiz")) {
-//       result = Session.get("currentQuiz");
-//     } else {
-//       result = this.getRandomSubset(this.getQuestions(), 10);
-//       Session.set("currentQuiz", result);
-//     }
-// 
-//     return result;
-//   },
-//   
-//   getQuestions: function() {
-//     // move this to a meteor method on the server
-//     return Questions.find({language: "javascript", difficulty: "easy"}).fetch();
-//   },
-//   
-//   getRandomSubset: function(set, count) {
-//     return _.first(_.shuffle(set), count);
-//   },
-//   
-// //  incrementCurrentQuestion: function() {
-// //    if(this.questionsRemain) {
-// //      Session.set("currentQuestion", this.nextQuestionIndex);
-// //    }
-// //  },
-//   
-//   questionsRemain: function() {
-//     Session.get("currentQuestion") <= (this.currentQuiz.length - 1)
-//   },
-//   
-//   nextQuestionIndex: function() {
-//     Session.get("currentQuestion") + 1
-//   }
-// }
-
-  // beginQuiz: function() {
-  //   Session.set("currentQuestion", 0);
-  // },
-
 Template.currentQuestion.helpers({
   question: function() {
-    return Quiz.getCurrentQuestion();
-    
-    return Session.get("currentQuestion");
+    return Quiz.getQuestion();
   }
 });
-
-Quiz = {
-  getCurrentQuestion: function() {
-    Session.set("currentQuestion", this.currentQuiz()[this.currentQuestionIndex()]);
-    return Session.get("currentQuestion");
-  },
-
-  currentQuestionIndex: function() {
-    return (Session.get("currentQuestionIndex") || 0);
-  },
-
-  currentQuiz: function() {
-    Session.get("currentQuiz") || Session.set("currentQuiz", [{ 
-      text: "Which method returns a string that might 'LOOK LIKE THIS?'",
-      correctAnswer: "toUpperCase",
-      isMultipleChoice: false,
-      difficulty: "easy",
-      language: "javascript"
-    }, {
-      text: "Which method extracts a section of a string and returns a new string?",
-      possibleAnswers: ["splice", "split", "slice", "shift"],
-      correctAnswer: "slice",
-      isMultipleChoice: true,
-      difficulty: "easy",
-      language: "javascript"
-    }]);
-    
-    return Session.get("currentQuiz");   
-  },
-
-  isCorrect: function(response) {
-    correctAnswer = Session.get("currentQuestion").correctAnswer;
-    return (response === correctAnswer);
-  },
-
-  incrementCurrentQuestion: function() {
-    this.incrementCurrentQuestionIndex();
-    Session.set("currentQuestion", this.currentQuiz()[this.currentQuestionIndex()]);
-  },
-
-  currentQuestionIndex: function() {
-    return (Session.get("currentQuestionIndex") || 0);
-  },
-
-  incrementCurrentQuestionIndex: function() {
-    var nextIndex = (this.currentQuestionIndex() + 1);
-
-    if(this.questionsRemain()) {
-      Session.set("currentQuestionIndex", nextIndex);
-    }
-  },
-
-  questionsRemain: function() {
-    return (this.currentQuestionIndex() < (this.currentQuiz().length - 1));
-  }
-}
 
 Template.currentQuestion.events({
   "click .submit": function(e) {
@@ -124,7 +10,7 @@ Template.currentQuestion.events({
     var response = e.target.value;
     var result = Quiz.isCorrect(response);
     console.log(result);
-    Quiz.incrementCurrentQuestion();
+    Quiz.incrementQuestion();
     
     return result;
   },
@@ -134,41 +20,73 @@ Template.currentQuestion.events({
     var response = $('#fitb-answer').val().trim();
     var result = Quiz.isCorrect(response);
     console.log(result);
-    Quiz.incrementCurrentQuestion();
+    Quiz.incrementQuestion();
 
     return Quiz.isCorrect(response);
   }
 });
 
-// Template.quiz.quizQuestions = function() {
-//   var questions = Questions.find({language: "javascript", difficulty: "easy"}).fetch();
+Quiz = {
+  getQuestion: function() {
+    return this.currentQuiz()[this.questionIndex()];
+  },
 
-//   return _.first(_.shuffle(questions), 10);
-// };
+  currentQuiz: function() {
+    return this.getOrInitializeQuiz();
+  },
 
-// Session.set("currentQuestion", 1);
+  getOrInitializeQuiz: function() {
+    if( !this.isQuizInitialized() ) {
+      this.initializeQuiz();
+    }
 
-// Template.currentQuestion.helpers({
-// })
+    return Session.get("currentQuiz");
+  },
 
-// Template.currentQuestion.events({
-//   "click submit": function(e){
-//     e.preventDefault();
-//     alert("this is the right submit");
-// // check answer
-//     Session.set("currentQuestion", Session.get("currentQuestion") + 1);
-//   }
-// });
+  isQuizInitialized: function() {
+    var quiz = Session.get("currentQuiz")
+    return (Array.isArray(quiz) && quiz.length > 0);
+  },
 
-//entire quiz collection
+  initializeQuiz: function() {
+    Session.set("currentQuiz", this.getQuiz());
+  },
+
+  getQuiz: function() {
+    var scope = Questions.find({language: "javascript", difficulty: "easy"});
+    var questions = scope.fetch();
+
+    return this.getRandomSubset(questions, 10);
+  },
+   
+   getRandomSubset: function(set, count) {
+     return _.first(_.shuffle(set), count);
+   },
+
+  questionIndex: function() {
+    return (Session.get("questionIndex") || 0);
+  },
+
+  incrementQuestion: function() {
+    var nextIndex = (this.questionIndex() + 1);
+
+    if(this.questionsRemain()) {
+      Session.set("questionIndex", nextIndex);
+    }
+  },
+
+  questionsRemain: function() {
+    return (this.questionIndex() < (this.currentQuiz().length - 1));
+  },
+
+  isCorrect: function(response) {
+    correctAnswer = this.getQuestion().correctAnswer;
+    return (response === correctAnswer);
+  }
+}
+
 //  1) get an array of 10 question objects at random from main quiz collection
 //  2) ->> currentQuestion
 //  3) # in the array
 //  4) correctAnswers ++
 //  5) Incorrect = wrongAnswers.push(currentQuestion)
-//
-// var quiz = {
-//  user_id:
-//  questions: X number of questions randomly chosen from the Collection
-//  answers: for each of the above questions
-// }
