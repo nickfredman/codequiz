19f6ed90029f9048faa72b9a81d3b53f5d9b582b
Nick Fredman <ntfredman@gmail.com>
2015-03-18 21:35:42 -0600

    Minor bug fixes across the site

--- client/templates/quiz/current_question.js
+++ client/templates/quiz/current_question.js
@@ -17,24 +17,35 @@ Template.currentQuestion.helpers({
 
 });
 
+// Template.currentQuestion.rendered({
+//   $('')
+// })
 Template.currentQuestion.events({
   "click .submit": function(e) {
     e.preventDefault();
     var response = e.target.value;
     var result = Quiz.isCorrect(response);
     if(result) {
-      $('.correct-flash').fadeTo(1500, 1).fadeTo(1500, 0);
+      $('.correct-flash').fadeTo(500, 1).fadeTo(2500, 0);
+      if(Quiz.questionsRemain()){
+        Meteor.setTimeout(function(){
+          Quiz.incrementQuestion();
+          return Quiz.incrementCorrect(result);
+        }, 750);
+      } else {
+        $('#final-modal').modal('show');
+      }
     } else if(result === false){
-      $('.incorrect-flash').fadeTo(1500, 1).fadeTo(1500, 0);
+      $('.incorrect-flash').fadeTo(500, 1).fadeTo(2500, 0);
+      if(Quiz.questionsRemain()){
+        Meteor.setTimeout(function(){
+        Quiz.incrementQuestion();
+        return Quiz.incrementCorrect(result);
+        }, 750);
+      } else {
+        $('#final-modal').modal('show');
+      }
     }
-    if(Quiz.questionsRemain()){
-      Quiz.incrementQuestion();
-      return Quiz.incrementCorrect(result);
-    } else {
-      $('#final-modal').modal('show');
-    }
-
-
   },
 
   "submit .fitb-form": function(e) {
@@ -42,17 +53,28 @@ Template.currentQuestion.events({
     var response = $('#fitb-answer').val().trim();
     var result = Quiz.isCorrect(response);
     if(result) {
-      $('.correct-flash').fadeTo(1500, 1).fadeTo(1500, 0);
+      $('.correct-flash').fadeTo(500, 1).fadeTo(2500, 0);
+      if(Quiz.questionsRemain()){
+        Meteor.setTimeout(function(){
+        Quiz.incrementQuestion();
+        return Quiz.incrementCorrect(result);
+        }, 750);
+      } else {
+        $('#final-modal').modal('show');
+      }
     } else if(result === false){
-      $('.incorrect-flash').fadeTo(1500, 1).fadeTo(1500, 0);
+      $('.incorrect-flash').fadeTo(500, 1).fadeTo(2500, 0);
+      if(Quiz.questionsRemain()){
+        Meteor.setTimeout(function(){
+        Quiz.incrementQuestion();
+        return Quiz.incrementCorrect(result);
+        }, 750);
+      } else {
+        $('#final-modal').modal('show');
     }
-    if(Quiz.questionsRemain()){
-      Quiz.incrementQuestion();
-      return Quiz.incrementCorrect(result);
-    } else {
-      $('#final-modal').modal('show');
-    }
-  },
+    $('#fitb-answer').val('');
+  }
+}
 
   // "click button": function(e) {
   //   e.preventDefault();
